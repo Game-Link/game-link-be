634f557f4357e107f18ee99b98597786740a8a25
ChaHyeonMin <gutanbug53@gmail.com>
2024-09-04 23:25:32 +0900

    feat : 권한 관련 AOP 추가

---
+++ src/main/java/com/gamelink/backend/global/auth/role/GuestAuth.java
@@ -0,0 +1,15 @@
+package com.gamelink.backend.global.auth.role;
+
+import com.gamelink.backend.global.auth.jwt.JwtProvider;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@SecurityRequirement(name = JwtProvider.AUTHORIZATION)
+@PreAuthorize("@RoleService.matchWithRole('ROLE_GUEST')")
+public @interface GuestAuth {
+}
---
+++ src/main/java/com/gamelink/backend/global/auth/role/service/RoleService.java
@@ -0,0 +1,5 @@
+package com.gamelink.backend.global.auth.role.service;
+
+public interface RoleService {
+    boolean matchWithRole(String role);
+}
