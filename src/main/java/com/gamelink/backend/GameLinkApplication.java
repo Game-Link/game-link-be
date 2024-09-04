823539fc667e4408db0e76d1dc5bdc2e376988cc
ChaHyeonMin <gutanbug53@gmail.com>
2024-09-04 17:31:10 +0900

    Initial Commit

---
+++ src/main/java/com/gamelink/backend/GameLinkApplication.java
@@ -0,0 +1,13 @@
+package com.gamelink.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameLinkApplication.class, args);
+	}
+
+}
