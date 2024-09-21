c79de837ce5d4089541453623f4e9416338756f2
ChaHyeonMin <gutanbug53@gmail.com>
2024-09-21 22:13:55 +0900

    remove: 기존에 사용하던 Repository 삭제

--- src/main/java/com/gamelink/backend/infra/riot/repository/SoloRankRepository.java
+++
@@ -1,8 +0,0 @@
-package com.gamelink.backend.infra.riot.repository;
-
-import com.gamelink.backend.global.base.CustomJpaRepository;
-import com.gamelink.backend.infra.riot.model.entity.queuetype.SoloRank;
-import com.gamelink.backend.infra.riot.repository.custom.CustomSoloRankRepository;
-
-public interface SoloRankRepository extends CustomJpaRepository<SoloRank, Long>, CustomSoloRankRepository {
-}
--- src/main/java/com/gamelink/backend/infra/riot/repository/TeamRankRepository.java
+++
@@ -1,8 +0,0 @@
-package com.gamelink.backend.infra.riot.repository;
-
-import com.gamelink.backend.global.base.CustomJpaRepository;
-import com.gamelink.backend.infra.riot.model.entity.queuetype.TeamRank;
-import com.gamelink.backend.infra.riot.repository.custom.CustomTeamRankRepository;
-
-public interface TeamRankRepository extends CustomJpaRepository<TeamRank, Long>, CustomTeamRankRepository {
-}
