1748d459cfadc3f1f11040ab641b8a7df5e27b7b
ChaHyeonMin <gutanbug53@gmail.com>
2024-10-08 14:33:34 +0900

    feat : 유저 프로필 레포지토리 추가

---
+++ src/main/java/com/gamelink/backend/domain/user/repository/ProfileImageRepository.java
@@ -0,0 +1,7 @@
+package com.gamelink.backend.domain.user.repository;
+
+import com.gamelink.backend.domain.user.model.entity.ProfileImage;
+import com.gamelink.backend.global.base.CustomJpaRepository;
+
+public interface ProfileImageRepository extends CustomJpaRepository<ProfileImage, Long> {
+}
