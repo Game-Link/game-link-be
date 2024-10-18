8e29d2761f4c56feccc0763a738e1f9401e86a93
ChaHyeonMin <gutanbug53@gmail.com>
2024-10-18 21:21:29 +0900

    feat : 캐싱에 필요한 key 지정

--- src/main/java/com/gamelink/backend/global/config/redis/RedisKeys.java
+++ src/main/java/com/gamelink/backend/global/config/redis/RedisKeys.java
@@ -4,6 +4,16 @@ public class RedisKeys {
     public static final String KEY_DELIMITER = ":";
 
     public static final String MATCH_ID_KEY = "match_id";
+    public static final String SOLO_RANK_MATCH_ID_KEY = "solo_rank_match_id";
+    public static final String TEAM_RANK_MATCH_ID_KEY = "team_rank_match_id";
+
+    public static final String TOTAL_KDA_KEY = "kda_id";
+    public static final String SOLO_KDA_KEY = "solo_kda_id";
+    public static final String TEAM_KDA_KEY = "team_kda_id";
+
+    public static final String TOTAL_CHAMPION_KEY = "champion_id";
+    public static final String SOLO_CHAMPION_KEY = "solo_champion_id";
+    public static final String TEAM_CHAMPION_KEY = "team_champion_id";
 
     public static String combine(Object key1, Object key2) {
         return key1 + KEY_DELIMITER + key2;
